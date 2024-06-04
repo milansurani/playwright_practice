594adcf19097c243a5fa0a37c8a839f6fda2aa1c
milansurani <milan79patel@gmail.com>
2024-06-04 17:34:08 -0400

    renamed test files and added new tests for UI controls

---
+++ tests/Login.spec.js
@@ -0,0 +1,50 @@
+const {test,expect} = require ('@playwright/test') ;
+
+
+// test cases
+test ('Log in with correct credentials', async ({page}) =>
+{
+    //navigate to the URL
+     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    //get the page title
+     await page.title();
+     //assert page title to be equals the value
+     await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
+     
+     //clear the username and write the correct value
+     await page.locator("#username").clear();
+     await page.locator("#username").fill("rahulshettyacademy");
+
+      //clear the password and write the correct value
+      await page.locator("#password").clear();
+      await page.locator("#password").fill("learning");
+
+     //click on signin button
+     await page.locator("#signInBtn");
+
+});
+
+test('Log in with incorrect credentials and verify the error message', async ({page}) =>
+{
+    //navigate to the URL
+     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    //get the page title
+     await page.title();
+     //assert page title to be equals the value
+     await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
+     
+     //clear the username and write the correct value
+     await page.locator("#username").clear();
+     await page.locator("#username").fill("rahulshetty");
+
+      //clear the password and write the correct value
+     await page.locator("#password").clear();
+     await page.locator("#password").fill("learning");
+
+     //click on signin button
+     await page.locator("#signInBtn").click();
+
+     //check for error message block
+     await expect(page.locator("[style*='block']")).toContainText("Incorrect");
+
+});
--- tests/UItest.spec.js
+++ tests/UItest.spec.js
@@ -1,50 +1,38 @@
 const {test,expect} = require ('@playwright/test') ;
 
+//test suite for UI controls
+test.describe('UI Controls Suite', () => {
 
-// test cases
-test ('Log in with correct credentials', async ({page}) =>
-{
-    //navigate to the URL
-     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-    //get the page title
-     await page.title();
-     //assert page title to be equals the value
-     await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
-     
-     //clear the username and write the correct value
-     await page.locator("#username").clear();
-     await page.locator("#username").fill("rahulshettyacademy");
+    // test cases
+test ('UI controls on Login Page', async ({page}) =>
+    {
+        //navigate to the URL
+         await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+        //get the page title
+         await page.title();
+         //assert page title to be equals the value
+         await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
+         
+         //clear the username and write the correct value
+         await page.locator("#username").clear();
+         await page.locator("#username").fill("rahulshettyacademy");
+    
+          //clear the password and write the correct value
+          await page.locator("#password").clear();
+          await page.locator("#password").fill("learning");
 
-      //clear the password and write the correct value
-      await page.locator("#password").clear();
-      await page.locator("#password").fill("learning");
+          //check the user radio button
+          await page.locator("radiotextsty").last().click();
+          await page.locator("#okayBtn").click();
+          expect(page.locator("radiotextsty").last()).toBeChecked();
 
-     //click on signin button
-     await page.locator("#signInBtn");
+          //select the correct value from dropdown
+          const dropdown = page.locator("select.form-control");
+          await dropdown.selectOption("consult");
+    
+         //click on signin button
+         await page.locator("#signInBtn");
+    
+    });
 
-});
-
-test.only ('Log in with incorrect credentials and verify the error message', async ({page}) =>
-{
-    //navigate to the URL
-     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-    //get the page title
-     await page.title();
-     //assert page title to be equals the value
-     await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
-     
-     //clear the username and write the correct value
-     await page.locator("#username").clear();
-     await page.locator("#username").fill("rahulshetty");
-
-      //clear the password and write the correct value
-     await page.locator("#password").clear();
-     await page.locator("#password").fill("learning");
-
-     //click on signin button
-     await page.locator("#signInBtn").click();
-
-     //check for error message block
-     await expect(page.locator("[style*='block']")).toContainText("Incorrect");
-
-});
+})
