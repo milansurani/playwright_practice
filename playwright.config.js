843ff969bb60c42f9d2b8f3ad5157931dc0b7714
milansurani <milan79patel@gmail.com>
2024-06-06 15:55:49 -0400

    added spec for child window test

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,10 @@ module.exports = defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    browserName : 'firefox',
+    trace: 'on',
+    screenshot: 'on',
+    headless : true
   },
 
   /* Configure projects for major browsers */
